ac5efaa45e9d0e39fa82f586e9dd1726eb0f186b
Irvus <iatarasenko@edu.hse.ru>
2021-03-04 22:49:57 +0300

    Убит мертвый пользователь v3.0

--- app/sessions_module/userSessions.py
+++ app/sessions_module/userSessions.py
@@ -1,22 +1,26 @@
 from app.sessions_module.userSession import UserSession
+
+
 class UserSessions():
     def __init__(self):
-        self._sessions  = []
-    
+        self._sessions = []
+
     def addUserSesion(self, user):
         self._sessions.append(UserSession(user))
         return self._sessions[-1].token
-    
+
     def existSessionWithToken(self, token):
         for session in self._sessions:
-            if not session.isActive():
+            if session.isActive():
+                if session.token == token:
+                    return True
+            else:
                 del session
-            if session.token == token:
-                return True
+
         return False
 
     def getSessionByToken(self, token):
         for session in self._sessions:
             if session.token == token:
                 return session
-        assert(False)
+        assert (False)
