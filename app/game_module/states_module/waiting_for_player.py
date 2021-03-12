13c5c6971ee0ab1eaf21527893e74e7bae976789
Zergey Loshkarev <zeryoshka@gmail.com>
2021-03-12 15:54:10 +0300

    Добавил docstrings к waiting before preparing

--- app/game_module/states_module/waiting_for_player.py
+++ app/game_module/states_module/waiting_for_player.py
@@ -10,11 +10,17 @@ class Waiting_for_player:
     It's a class for state WAITING_FOR_PLAYER
     '''
     def __init__(self, game):
+        '''
+        Init Method for class Waiting for player
+        '''
         self.game = game
         self._sub_state = WAITING_FOR_PLAYER__WAIT
         self.counterDown = CounterDown(COUNT_DOWN_BEFORE_PREPARING__TIME)
 
     def addPlayer(self, userSession):
+        '''
+        Method for add user to game
+        '''
         self.game.players.append(Player(userSession))
 
     def needMorePlayer(self):
@@ -28,21 +34,33 @@ class Waiting_for_player:
         return self.counterDown.started
 
     def __getattribute__(self, name):
+        '''
+        It needs for check and make assert in case of error
+        when game.players_count > self.game.needPlayersCount
+        '''
         if name not in ('game'):
             assert(self.game.players_count <= self.game.needPlayersCount)
         return object.__getattribute__(self, name)
 
     def delPlayersWithDiedSession(self):
+        '''
+        Method for delete user with_died session form players
+        '''
         for i, player in enumerate(self.game.players):
             if not player.userSession.isActive():
                 del player
 
     def state(self, state):
+        '''
+        Method for check sub_state of class state
+        '''
         return self._sub_state == state
 
     def close_state(self):
+        '''
+        It needs for close state in case end of state
+        '''
         if self.counterDown.finished and self.game.state(WAITING_FOR_PLAYER):
             self.game.next_state()
-            print('****************************************************************************************************************')
 
         return self.game.state(WAITING_FOR_PLAYER)
