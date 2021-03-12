daceaf58af7bd20a964082fd6a34a67d3b6bea43
Zergey Loshkarev <zeryoshka@gmail.com>
2021-03-12 15:11:43 +0300

    Add doc-strings to Preparing_for_game

--- app/game_module/states_module/preparing_for_game.py
+++ app/game_module/states_module/preparing_for_game.py
@@ -15,10 +15,17 @@ class Preparing_for_game:
         self.counterDown = CounterDown(PREPARING_FOR_GAME__TIME)
 
     def start(self):
+        '''
+        Method for start current state,
+        actually it need for activate counterDown inside this object
+        '''
         if self.game.state(PREPARING_FOR_GAME):
             self.counterDown.start()
 
     def close_state(self):
+        '''
+        Method for close current state and set next state
+        '''
         if self.counterDown.finished and self.game.state(PREPARING_FOR_GAME):
             self.game.next_state()
             print('****************************************************************************************************************')
