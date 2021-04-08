d62b04dc66ce691a8cb864265e8dad1dbc4a07aa
IAT <iatarasenko@edu.hse.ru>
2021-04-09 00:43:01 +0300

    Добавлен парсинг csv, дошлифован лот, проект не сломан

--- app/auction_module/auction.py
+++ app/auction_module/auction.py
@@ -1,5 +1,6 @@
 from app.auction_module.lot import Lot
 import json
+import csv
 
 
 class Auction:
@@ -7,28 +8,41 @@ class Auction:
         self.actual_lots = []
         self.sold_lots = []
 
-    def parsing(self):  # TODO занесение лотов текущего аукциона в actual_lots
+    def parsing(self):
+        csv_name = 'app/auction_module/test.csv'
+        with open(csv_name) as csv_file:
+            tmp = csv.reader(csv_file)
+
+            for row in tmp:
+                self.actual_lots.append(Lot(*row))
+
+            self.actual_lots.pop(0)
+
+            for lot in self.actual_lots:
+                print(lot.return_info())
+
         for lot in self.actual_lots:
-            if lot.lot_type == 'generator':
+            if lot.lot_type == 'producer':
                 lot.min_cost = 1
                 lot.max_cost = 100
-            elif lot.lot_type == 'user':
+            elif lot.lot_type == 'consumer':
                 lot.min_cost = 1
                 lot.max_cost = 10
             else:
-                assert 'Херня данные'
+                assert 'Кривые данные'
 
     def export_as_json(self):
         return json.dumps(self.actual_lots + self.sold_lots)
 
     def analysing_bets(self, json_str):  # в строке "id игрока": "ставка"
-        self.actual_lots[0].who_can_bid = list(json_str.keys)  # тут вообще словарь, а еще не работает так, как нужно, исправить
+        self.actual_lots[0].who_can_bid = list(
+            json_str.keys)  # тут вообще словарь, а еще не работает так, как нужно, исправить
         pass  # хитрый анализ с выбором достойного
 
     def end_auction(self):
         self.actual_lots[0].status = 'sold'
         self.sold_lots.append(self.actual_lots[0])
-        del(self.actual_lots[0])
+        del (self.actual_lots[0])
 
     def return_lots_list(self):
         x = []
@@ -39,3 +53,5 @@ class Auction:
             x.append(lot.return_info())
 
         return x
+
+
--- app/auction_module/lot.py
+++ app/auction_module/lot.py
@@ -1,23 +1,21 @@
-import json
-
+from app.sessions_module.userSessions import UserSessions
 
 class Lot:
-    def __init__(self, lot_id, lot_name, lot_type, lot_params, min_cost, min_start_cost, max_cost, max_start_cost,
-                 auction_type, who_can_bid):
-        self.lot_id = lot_id
+    def __init__(self, id, name, lot_type, auction_type, min_cost, max_cost):
+        self.id = id
         self.status = 'not sold'
-        self.lot_name = lot_name
+        self.name = name
         self.lot_type = lot_type
-        self.lot_params = lot_params
-        self.if_purchased = False
-        self.who_bought = ''
-        self.purchase_cost = 0
-        self.min_cost = min_cost
-        self.min_start_cost = min_start_cost
-        self.max_cost = max_cost
-        self.max_start_cost = max_start_cost
         self.auction_type = auction_type
-        self.who_can_bid = who_can_bid
+        self.is_current = False
+        self.is_purchased = False
+        self.who_bought = None
+        self.purchase_cost = None
+        self.min_cost = min_cost
+        self.min_start_cost = min_cost
+        self.max_cost = max_cost
+        self.max_start_cost = max_cost
+        self.who_can_bid = UserSessions.exportSessions()
 
     def is_not_sold(self):
         if self.status == 'not sold':
@@ -41,14 +39,13 @@ class Lot:
         pass
 
     def return_info(self):
-        return {'lot_id': self.lot_id, 'status': self.status, 'lot_name': self.lot_name, 'lot_type': self.lot_type,
-                'lot_params': self.lot_params, 'if_purchased': self.if_purchased, 'who_bought': self.who_bought,
+        return {'lot_id': self.id, 'status': self.status, 'lot_name': self.name, 'lot_type': self.lot_type,
+                'is_current': self.is_current, 'is_purchased': self.is_purchased, 'who_bought': self.who_bought,
                 'purchase_cost': self.purchase_cost, 'min_cost': self.min_cost, 'min_start_cost': self.min_start_cost,
                 'max_cost': self.max_cost, 'max_start_cost': self.max_start_cost, 'auction_type': self.auction_type,
                 'who_can_bid': self.who_can_bid}
 
 
 if __name__ == "__main__":
-    lot1 = Lot(1223, 'bepis', 'user', [1, 3, 3], 100, 1200, 10000,
-               1000000, 'english', [1, 2, 3])
+    lot1 = Lot(1223, 'user', [1, 3, 3], 100, 10000, 'english')
     print(lot1.return_info())
