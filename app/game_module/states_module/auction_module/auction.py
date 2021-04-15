4c5e7a01327c71f561b6f1e9da215f76b1e7a1d0
Ilia <iatarasenko@edu.hse.ru>
2021-04-15 23:43:41 +0300

    Починил экспорт данных

--- app/game_module/states_module/auction_module/auction.py
+++ app/game_module/states_module/auction_module/auction.py
@@ -31,7 +31,7 @@ class Auction:
 
     def parsing(self):  # парсинг из цсв в список и предварительная обработка лотов
         csv_name = 'app/auction_module/test.csv' # TODO: Убери в конфиги, не позорься!!!
-        with open(csv_name, encoding='utf-8') as csv_file:
+        with open(csv_name, encoding='utf-8') as csv_file:  ## Да не позорюсь я, это заготовка!!!
             tmp = csv.DictReader(csv_file)
             for row in tmp:
                 self.actual_lots.append(Lot(**row))
@@ -58,7 +58,7 @@ class Auction:
                 # И преобразовать cost и всякое такое к числам
                 # Можешь отдельный модуль конфигов пределать к текущему пакету конфигов, чтобы был чисто для аукциона
 
-    def export_as_json(self):  # возвращает json со всей инфой о каждом лоте
+    def export_data(self):  # возвращает json со всей инфой о каждом лоте
         x = []
         for lot in self.actual_lots:
             x.append(lot.return_info())
@@ -66,7 +66,7 @@ class Auction:
         for lot in self.sold_lots:
             x.append(lot.return_info())
 
-        return json.dumps(x)
+        return x
 
     #TODO Это правильнее будет запихать в сам лот,
     #метод будет возвращать список юзеров, которые могут дальше бодаться
