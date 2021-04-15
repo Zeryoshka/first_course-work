ee764fb009cb05bdafa859739c28a91cf9053c4d
Zergey Loshkarev <zeryoshka@gmail.com>
2021-04-15 19:45:20 +0300

    Добавил todo и свою ругань

--- app/auction_module/auction.py
+++ app/auction_module/auction.py
@@ -2,7 +2,13 @@ from app.auction_module.lot import Lot
 import json
 import csv
 
-
+# TODO аукцион это state для game поэтому он должен лежать в соответствующем пакете
+# Метод start для ацкциона, чтобы начинать 
+# Иметь метод close_state для перехода game к следующему состоянию
+# Иметь возможность розыгргыша лота, возможность начать его продавать
+# А обратный отсчет где? (Там для этог есть класс)
+# А как ставку сдлеать?
+# А где render_param, чтобы параметры для страничек делать?
 class Auction:
     def __init__(self):
         self.actual_lots = []
@@ -39,10 +45,10 @@ class Auction:
         return json.dumps(x)
 
     def analysing_bets(self, bids):  # в приходящем словаре "id игрока": "ставка"
-        self.actual_lots[0].who_can_bid = list(bids.keys)
+        self.actual_lots[0].who_can_bid = list(bids.keys) # Почему 0!?!?!?!?!?!?
         # хитрый анализ с выбором достойного. Мне страшно за него браться
 
     def end_auction(self):  # метод завершения розыгрыша лота
-        self.actual_lots[0].make_lot_sold()
-        self.sold_lots.append(self.actual_lots[0])
-        del (self.actual_lots[0])
+        self.actual_lots[0].make_lot_sold()# Почему 0!?!?!?!?!?!?
+        self.sold_lots.append(self.actual_lots[0])# Почему 0!?!?!?!?!?!?
+        del (self.actual_lots[0])# Почему 0!?!?!?!?!?!?
