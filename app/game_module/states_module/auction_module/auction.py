51f3fc9d8f99b468c8add625d1022f6d089fecb8
Zergey Loshkarev <zeryoshka@gmail.com>
2021-04-23 15:36:34 +0300

    Вписал docsring из комментариев

--- app/game_module/states_module/auction_module/auction.py
+++ app/game_module/states_module/auction_module/auction.py
@@ -34,19 +34,24 @@ class Auction:
     def cur_lot(self):
         return self.actual_lots[self._cur_lot_num]
 
-    def parsing(self):  # парсинг из csv в список и предварительная обработка лотов
+    def parsing(self):
+        '''
+        парсинг из csv в список и предварительная обработка лотов
+        '''
         csv_name = 'app/auction_module/test.csv'  # TODO: Убери в конфиги, не позорься!!!
         with open(csv_name, encoding='utf-8') as csv_file:
             tmp = csv.DictReader(csv_file)
             for row in tmp:
                 self.actual_lots.append(Lot(self.game.players,**row))
-                
                 # А еще надо проверить тип аукциона, это же тоже строка!!
                 # Еще все параметры, кроме разве что name
                 # И преобразовать cost и всякое такое к числам
                 # Можешь отдельный модуль конфигов пределать к текущему пакету конфигов, чтобы был чисто для аукциона
 
-    def export_data(self):  # возвращает список со всей инфой о каждом актуальном лоте
+    def export_data(self): 
+        '''
+        возвращает список со всей инфой о каждом актуальном лоте
+        '''
         x = []
         for lot in self.actual_lots:
             x.append(lot.return_info())
@@ -57,7 +62,10 @@ class Auction:
     # метод будет возвращать список юзеров, которые могут дальше бодаться
     # Причем в make_lot_sold()
 
-    def analysing_bets(self, bids):  # в приходящем словаре "id игрока": "ставка"
+    def analysing_bets(self, bids): 
+        '''
+        в приходящем словаре "id игрока": "ставка"
+        '''
         self.actual_lots[0].who_can_bid = list(bids.keys)
         # хитрый анализ с выбором достойного. Мне страшно за него браться
 
@@ -80,7 +88,10 @@ class Auction:
         if self.cur_lot.valid(player, price):
             self.cur_lot.add_bet(player, price)
 
-    def sell_lot(self): # метод завершения розыгрыша лота
+    def sell_lot(self):
+        '''
+        метод завершения розыгрыша лота
+        '''
         users_in_game = self.actual_lots[self._cur_lot_num].make_lot_sold() # Почему 0!?!?!?!?!?!?
         if len(users_in_game) == 1:
             self._cur_lot_num += 1
