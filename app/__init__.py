11a81f4076d431e84b822d21080ff33083ed3f52
Zergey Loshkarev <zeryoshka@gmail.com>
2021-04-24 20:15:03 +0300

    Мозговыносительные правки, без которых не будет работать

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 
 from app.config_module.base_config import SECRET_KEY, STATIC_FOLDER, TEMPLATE_FOLDER
-from app.game_module.game import Game
-from app.users_module.users import Users
-from app.sessions_module.userSessions import UserSessions
 
 app = Flask(__name__)
 
@@ -11,8 +8,13 @@ app.secret_key = SECRET_KEY
 app.static_folder = STATIC_FOLDER
 app.template_folder = TEMPLATE_FOLDER
 
+from app.sessions_module.userSessions import UserSessions
 userSessions = UserSessions()
+
+from app.users_module.users import Users
 users = Users()
+
+from app.game_module.game import Game
 game = Game()
 
 from app import routes
