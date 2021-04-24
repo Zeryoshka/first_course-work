82b77620ef089e6c090f2f82ee72ded25e0957b5
Zergey Loshkarev <zeryoshka@gmail.com>
2021-04-24 13:16:23 +0300

    Правки для конфигов

--- app/config_module/base_config.py
+++ app/config_module/base_config.py
@@ -2,9 +2,11 @@ from datetime import timedelta
 
 SECRET_KEY = 'KEY_KEY'  # Ключ для flask.session
 
+NEED_PLAYERS_COUNT = 1# Количество игроков
+
 # Константы времени (в секундах)
-COUNT_DOWN_BEFORE_PREPARING__TIME = timedelta(seconds=10)
-PREPARING_FOR_GAME__TIME = timedelta(seconds=20)
+COUNT_DOWN_BEFORE_PREPARING__TIME = timedelta(seconds=1)
+PREPARING_FOR_GAME__TIME = timedelta(seconds=1)
 BET__TIME = timedelta(seconds=10)
 USERSESSION_LIFETIME = 5000  # Время жизни UserSession
 
