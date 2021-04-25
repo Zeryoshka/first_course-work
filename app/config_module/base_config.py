a5b14d82494ab93be3c37fcbdba0a60ac759fef1
Zergey Loshkarev <zeryoshka@gmail.com>
2021-04-25 17:52:47 +0300

    Обнулил первые два процесса, чтобы быстрее доходить до аукциона

--- app/config_module/base_config.py
+++ app/config_module/base_config.py
@@ -5,8 +5,8 @@ SECRET_KEY = 'KEY_KEY'  # Ключ для flask.session
 NEED_PLAYERS_COUNT = 1# Количество игроков
 
 # Константы времени (в секундах)
-COUNT_DOWN_BEFORE_PREPARING__TIME = timedelta(seconds=1)
-PREPARING_FOR_GAME__TIME = timedelta(seconds=1)
+COUNT_DOWN_BEFORE_PREPARING__TIME = timedelta(seconds=0)
+PREPARING_FOR_GAME__TIME = timedelta(seconds=0)
 BET__TIME = timedelta(seconds=10)
 USERSESSION_LIFETIME = 5000  # Время жизни UserSession
 
