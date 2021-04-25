7b8413cd424ecdd66190e5bb52c7da46fd464014
Zergey Loshkarev <zeryoshka@gmail.com>
2021-04-25 22:11:06 +0300

    Написал генерацию лота

--- app/static/javascript/auction.js
+++ app/static/javascript/auction.js
@@ -6,7 +6,7 @@ function send_bet(event) {
         alert('Ставка не удовлетворяет текущему условию');
         return false;
     }
-    lot_id = Number($('.cur_lot_id').text());
+    lot_id = Number($.cookie('cur_lot_id'));
     request_data = {
         lot_id: lot_id,
         price: cur_bet,
@@ -21,8 +21,58 @@ function send_bet(event) {
     return false;
 }
 
+/* <div class="lot 
+                    {% if lot.is_current %}
+                        lot_type_current
+                    {% elif lot.who_bought is none %}
+                        lot_owner_nobody
+                    {% elif lot.who_bought.name == user.name %}
+                        lot_owner_your
+                    {% elif lot.who_bought.name != user.name %}
+                        lot_owner_not-you
+                    {% endif %}
+                "
+                >
+                </div> */
+
+function create_lot(id, name, who_bought, price) {
+    var lot_id = $('<div></div>', {
+        class: 'lot__number',
+        text: id
+    });
+    var lot_name = $('<div></div>', {
+        class: 'lot__name',
+        text: name
+    });
+    var lot_team = $('<div></div>', {
+        class: 'lot__team',
+        text: who_bought
+    });
+    var lot_price = $('<div></div>', {
+        class: 'lot__price',
+        text: price
+    });
+    return $('<div></div>', {
+        class: 'lot__price',
+    }).append(lot_id, lot_name, name, who_bought, price);
+}
+
+function update_page(cur_lot, lots) {
+    $('.lots').empty();
+    for (var i = 0; i < lots.length; ++i){
+        $('div.')
+    }
+}
+
 function get_updates() {
     $.get('api/update_lots').done(function(response){
+        if (response.cur_lot.lot_id == $.cookie('cur_lot_id')) {
+            setTimeout(get_updates, 100);
+            return;
+        }
+        $.cookie('cur_lot_id', response.cur_lot.id)
+        tik_tak(response.left_time);
+        update_page(response.cur_lot, response.lots);
         console.log(response);
     });
 }
