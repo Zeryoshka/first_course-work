d9ffadc0f280f72a7e318eb5eee90a2d3599595f
Zergey Loshkarev <zeryoshka@gmail.com>
2021-05-01 19:32:25 +0300

    Исправил конфигурационный файл

--- app/config_module/base_config.py
+++ app/config_module/base_config.py
@@ -5,15 +5,14 @@ SECRET_KEY = 'KEY_KEY'  # Ключ для flask.session
 NEED_PLAYERS_COUNT = 2# Количество игроков
 
 # Константы времени (в секундах)
-COUNT_DOWN_BEFORE_PREPARING__TIME = timedelta(seconds=0)
-PREPARING_FOR_GAME__TIME = timedelta(seconds=0)
-BID__TIME = timedelta(seconds=10)
+COUNT_DOWN_BEFORE_PREPARING__TIME = timedelta(seconds=10)
+PREPARING_FOR_GAME__TIME = timedelta(seconds=10)
+BID__TIME = timedelta(seconds=15)
 USERSESSION_LIFETIME = 5000  # Время жизни UserSession
 
 # Состояния объекта Game (их коды)
 NONE_STATE = 0  # Не указано
 WAITING_FOR_PLAYER = 1  # Ожидание игроков
-COUNT_DOWN_BEFORE_PREPARING = 6  # Ожидание начала отсчета
 PREPARING_FOR_GAME = 2  # Подготовка к игре
 AUCTION = 3  # Аукцион
 EMULATION = 4  # Эмуляция
